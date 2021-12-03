a05c4d755c14be681ce2dd049a6e9e38dcbf2128
Karen Cabingatan <karen.cyantis@gmail.com>
2021-12-03 13:33:04 +0800

    removed routes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,13 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Landing } from "./Landing";
-import { Gallery } from "./Gallery";
-
 export class Home extends React.Component {
   render() {
     return (
       <>
-        <Router>
+      testing
+      <Landing/>
+        {/* <Router>
           <Routes>
             <Route exact path="/" element={<Landing />} />
             <Route
@@ -20,7 +20,7 @@ export class Home extends React.Component {
               }
             />
           </Routes>
-        </Router>
+        </Router> */}
       </>
     );
   }
