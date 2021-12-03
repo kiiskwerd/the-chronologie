4432bbbaf83c2ec5b0fd542b53e1bcc7611b3a6b
Karen Cabingatan <karen.cyantis@gmail.com>
2021-12-03 13:35:20 +0800

    cleanup

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,10 @@
 import React from "react";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Landing } from "./Landing";
 export class Home extends React.Component {
   render() {
     return (
       <>
-      testing
       <Landing/>
-        {/* <Router>
-          <Routes>
-            <Route exact path="/" element={<Landing />} />
-            <Route
-              path="/gallery"
-              element={
-                <>
-                  {" "}
-                  <Gallery /> <Landing />{" "}
-                </>
-              }
-            />
-          </Routes>
-        </Router> */}
       </>
     );
   }
